464a5c6feff769cbe3a747386e591b5a7e305717
HarryXu497 <harryxu497@gmail.com>
2023-05-02 15:30:14 -0400

    changes

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,13 +1,20 @@
 public abstract class GameObject {
     // The maximum health of the object, which is also the default health
     private static int DEFAULT_MAXIMUM_HEALTH = 100;
+
     // health of the object
     private int health;
+
+    // Position
+    private int x;
+    private int y;
 
     /**
      * constructs a new game object with a default health
      */
-    public GameObject() {
+    public GameObject(int x, int y) {
+        this.x = x;
+        this.y = y;
         this.health = DEFAULT_MAXIMUM_HEALTH;
     }
 
@@ -16,15 +23,17 @@ public abstract class GameObject {
      * @param health the custom health of the object
      * @throws IllegalArgumentException if the health is negative or larger than the maximum (100).
      */
-    public GameObject(int health) {
+    public GameObject(int x, int y, int health) {
         if (health < 0) {
             throw new IllegalArgumentException("health " + health + " is negative.");
         }
         if (health > DEFAULT_MAXIMUM_HEALTH) {
-            throw new IllegalArgumentException("health " + health + " is larger than the maximum health " + DEFAULT_MAXIMUM_HEALTH + ".")
+            throw new IllegalArgumentException("health " + health + " is larger than the maximum health " + DEFAULT_MAXIMUM_HEALTH + ".");
         }
 
         this.health = health;
+        this.x = x;
+        this.y = y;
     }
 
     /**
@@ -47,13 +56,21 @@ public abstract class GameObject {
             throw new IllegalArgumentException("health " + health + " is negative.");
         }
         if (health > DEFAULT_MAXIMUM_HEALTH) {
-            throw new IllegalArgumentException("health " + health + " is larger than the maximum health " + DEFAULT_MAXIMUM_HEALTH + ".")
+            throw new IllegalArgumentException("health " + health + " is larger than the maximum health " + DEFAULT_MAXIMUM_HEALTH + ".");
         }
 
         this.health = health;
     }
 
+    /**
+     * update
+     * called to update the state of the game object (i.e. called once every loop.
+     */
     public abstract void update();
 
+    /**
+     * collide
+     * called upon collision with another game object.
+     */
     public abstract void collide(GameObject other);
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,53 @@
-public class Person extends GameObject {
+public class Person extends GameObject implements Movable {
 
+    private int age;
+    private Sex sex;
+
+    public Person(int x, int y, Sex sex, int age) {
+        super(x, y);
+
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public Person(int x, int y, int health, Sex sex, int age) {
+        super(x, y, health);
+
+        this.sex = sex;
+        this.age = age;
+    }
+
+    @Override
+    public void update() {
+        this.age++;
+        this.setHealth(this.getHealth() - 1);
+    }
+
+    @Override
+    public void collide(GameObject other) {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    /**
+     * getSex
+     * returns the sex of this person
+     * @return the sex of this person as a Sex enum
+     */
+    public Sex getSex() {
+        return this.sex;
+    }
+
+    /**
+     * getAge
+     * returns the age of the person
+     * @return the age of the person as an integer
+     */
+    public int getAge() {
+        return this.age;
+    }
 }
